a6b49e0647087ae3df2030bb1c6891f2251f767b
Karim SAD <ksad.karim@gmail.com>
2018-02-10 21:11:00 +0100

    Improve packages downloading in mint install script

--- Config/mint_install.sh
+++ Config/mint_install.sh
@@ -37,12 +37,7 @@ wget -P ~/.ssh/ https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/s
 chmod 600 ~/.ssh/dell_mint*
 
 #bashrc
-wget https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/.bashrc
-cp .bashrc ~/.bashrc
-
-# Cinnamon settings
-wget https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/cinnamon_settings
-dconf load /org/cinnamon/ < cinnamon_settings
+wget -P ~/.bashrc https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/.bashrc
 
 # Create folder to mount iso images
 mkdir /media/iso
@@ -50,7 +45,7 @@ mkdir /media/iso
 # Download fillezilla config
 wget https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/FileZilla/FileZilla.xml
 
-apt-get install vim git gitk virtualbox shutter deluge gparted guake gthumb dconf-cli
+apt-get --assume-yes install vim git gitk virtualbox shutter deluge gparted guake gthumb dconf-cli filezilla unetbootin
 
 # git configuration
 git config --global core.editor "vim"
@@ -61,19 +56,18 @@ git config --global user.email ksad.karim@gmail.com
 # Mac OS theme
 add-apt-repository ppa:noobslab/macbuntu
 apt-get update
-apt-get install macbuntu-os-icons-lts-v7
-apt-get install macbuntu-os-ithemes-lts-v7
+apt-get --assume-yes install macbuntu-os-icons-lts-v7 macbuntu-os-ithemes-lts-v7
 
 
 # Slingscold Launchpad
 add-apt-repository ppa:noobslab/macbuntu
 apt-get update
-apt-get install slingscold
+apt-get --assume-yes install slingscold
 
 # Synapse 
 add-apt-repository ppa:synapse-core/ppa
 apt-get update
-apt-get install synapse
+apt-get --assume-yes install synapse
 
 # Skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
@@ -88,7 +82,7 @@ dpkg -i teamviewer_amd64.deb
 sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 apt-get update
-apt-get install google-chrome-stable
+apt-get --assume-yes install google-chrome-stable
 
 # FoxitReader
 wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x64_enu_Setup.run.tar.gz
@@ -96,4 +90,6 @@ tar xzvf FoxitReader*.tar.gz
 sudo chmod a+x FoxitReader*.run
 ./FoxitReader.*.run
 
-
+# Cinnamon settings
+wget https://raw.githubusercontent.com/ksad/Dev-Tools/master/Config/cinnamon_settings
+dconf load /org/cinnamon/ < cinnamon_settings
