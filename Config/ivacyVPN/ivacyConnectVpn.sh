2b9f7650985859f5db81579bc98e6da1f780ef5d
Karim S <ksad.karim@gmail.com>
2018-05-20 15:48:32 +0200

    Update ivacyConnectVpn.sh

--- Config/ivacyVPN/ivacyConnectVpn.sh
+++ Config/ivacyVPN/ivacyConnectVpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cyberpptp --add ivacyvpn vleu-be1.dns2use.com ksad.karim@gmail.com syrine2016
+sudo cyberpptp --add ivacyvpn vleu-be1.dns2use.com <email> <password>
 curl ipinfo.io
